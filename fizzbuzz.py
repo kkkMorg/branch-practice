1125c2f1324a0a8f1a527959997a1bc51a6ed546
kkkMorg <Morg.k.0104@gmail.com>
2025-11-13 16:46:55 +0900

    feat: Do fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,9 @@
-print('hello')
+# fizzbuzz
+# 1 ~ n 까지의 자연수에 대해, 3의 배수라면 fizz, 5 buzz, 15 fizzbuzz, 나머지는 숫자 
+
+for i in range(1, 15 + 1):
+
+    if i % 3 == 0 :
+        print('fizz')
+    else:
+        print(i)
